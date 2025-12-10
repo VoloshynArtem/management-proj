8e1dac4261f05312f6da5a6b0dc60e16f92abb06
VoloshynArtem <vartem.gg@gmail.com>
2025-12-10 17:58:21 +0100

    visual improvements

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -2,6 +2,8 @@ using Avalonia.Controls;
 using System;
 using Avalonia.Interactivity;
 using Avalonia.Layout;
+using Avalonia.Media;
+using Avalonia.Controls.Documents;
 
 using System.Collections;
 namespace managementProj;
@@ -25,11 +27,22 @@ public partial class MainWindow : Window{
       Con.Content = Panels [0];
   }
 
-
-
     public void ClickHandler(object sender, RoutedEventArgs args){
       Button clickedButton = sender as Button;
       Con.Content = Panels[int.Parse(clickedButton.Tag.ToString())];
       SelectionEvent.Invoke(clickedButton, EventArgs.Empty);
+
+      foreach(Button b in buttonGrid.Children){
+        if (b.Tag == clickedButton.Tag){
+          (b.Content as TextBlock).TextDecorations = TextDecorations.Underline;
+          (b.Content as TextBlock).FontWeight = FontWeight.Bold;
+        }else {
+          (b.Content as TextBlock).TextDecorations = null;
+          (b.Content as TextBlock).FontWeight = FontWeight.Normal;
+
+        }
+      }
     }
+
+
 }
